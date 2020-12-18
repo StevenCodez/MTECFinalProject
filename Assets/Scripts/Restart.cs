f41dadfa73f46aa990a34edfa07d1d273a4c3492
StevenCodez <Stevenjonlui@gmail.com>
2020-12-18 13:06:10 -0500

    Scene Offical

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -14,6 +14,9 @@ public class Restart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("FINAL");
+        }
     }
 }
